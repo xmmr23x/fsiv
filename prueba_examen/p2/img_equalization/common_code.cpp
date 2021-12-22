70fa1027284b64f67255ccda73ce9fa056ff28b4
i72marom <i72marom@uco.es>
2021-12-22 17:51:41 +0100

    commit automatico

--- prueba_examen/p2/img_equalization/common_code.cpp
+++ prueba_examen/p2/img_equalization/common_code.cpp
@@ -63,17 +63,10 @@ fsiv_create_equalization_lookup_table(const cv::Mat& hist,
 	lkt = hist.clone();
 	
 	fsiv_normalize_histogram(lkt);
-
-
-
-	for (int i = 1; i < hist.rows; ++i)
-		std::cout << hist.at<float>(i,0) << ":" << i << ":";
-	std::cout << std::endl;
-
 	fsiv_accumulate_histogram(lkt);
+
 	for (int i = 1; i < hist.rows; ++i)
-		std::cout << lkt.at<float>(i,0) << ":" << i << ":";
-	std::cout << std::endl;
+		std::cout << lkt.at<float>(i,0) << ":" << i << ":" << std::endl;
 
 	lkt.convertTo(lkt, CV_8UC1, 255.0);
 	//
