48f35cf9ea96b6546063e4e08b972c02b900fe6a
i72marom <i72marom@uco.es>
2021-12-22 13:43:23 +0100

    commit automatico

--- prueba_examen/p2/img_equalization/common_code.cpp
+++ prueba_examen/p2/img_equalization/common_code.cpp
@@ -31,7 +31,7 @@ fsiv_normalize_histogram(cv::Mat& hist)
     CV_Assert(hist.rows==256 && hist.cols==1);
 
     //TODO
-    cv::normalize(hist, hist);
+    cv::normalize(hist, hist, 1, 0, cv::NORM_L1);
 
     //
     CV_Assert(hist.type()==CV_32FC1);
